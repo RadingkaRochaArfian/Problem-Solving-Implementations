eae025deb8e996ccee408ef8417b4930a9fc1666
RadingkaRochaArfian <radingka.arfian@gmail.com>
2026-07-21 18:28:48 +0700

    add insert function to set testing

--- library/Set.cpp
+++ library/Set.cpp
@@ -1,11 +1,15 @@
 // set is ordered of unique values
-// to do:
-// find_if function
-// lower_bound function
+// to do function:
+// find_if
+// lower_bound
+// begin
+// rbegin
+// rend
+// empty
 #include <iostream>
 #include <set>
 using namespace std;
-void printSet(set<int> st) {
+void printSet(set<int> &st) {
   cout << "Current set values: {";
   bool flag = true;
   for (auto x : st) {
@@ -16,16 +20,22 @@ void printSet(set<int> st) {
   }
   cout << "}" << endl;
 }
-void fFind(set<int> st) {
+void fFind(set<int> &st) {
   auto it = st.find(01);
   if (it != st.end()) {
-    cout << "Deleting 1 on set..." << endl;
+    cout << "Deleting {1} on set..." << endl;
     st.erase(01);
   }
+  printSet(st);
+}
+void fInsert(set<int> st) {
+  cout << "Inserting {32} to set..." << endl;
+  st.insert(32);
   printSet(st);
 }
 int main() {
   set<int> st = {80, 77, 01, 99};
   printSet(st);
   fFind(st);
+  fInsert(st);
 }
