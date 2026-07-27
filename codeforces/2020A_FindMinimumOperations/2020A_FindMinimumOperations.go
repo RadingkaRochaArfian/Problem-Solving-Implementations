ea154f36774cd419acaacbd42b69c3ef2c959af8
RadingkaRochaArfian <radingka.arfian@gmail.com>
2026-07-27 22:41:26 +0700

    add 2020A implementation and link

---
+++ codeforces/2020A_FindMinimumOperations/2020A_FindMinimumOperations.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func printf(f string, a ...any) { fmt.Fprintf(writer, f, a...) }
+func scan(a ...any)             { fmt.Fscan(reader, a...) }
+func solve() {
+	var n, k int64
+	scan(&n, &k)
+	if k == 1 {
+		printf("%d\n", n)
+		return
+	}
+	ans := int64(0)
+	for n != 0 {
+		ans += n % k
+		n /= k
+	}
+	printf("%d\n", ans)
+}
+func main() {
+	defer writer.Flush()
+	var t int64
+	scan(&t)
+	for range t {
+		solve()
+	}
+}
