2aa374ad4703f5ec4ef1d555c90027c58978261c
RadingkaRochaArfian <radingka.arfian@gmail.com>
2026-08-02 00:06:14 +0700

    attempt to solve codeforces contest 1113 Div 2

--- temp/a.cpp
+++ temp/a.cpp
@@ -1,43 +1,28 @@
-#include <algorithm>
-#include <climits>
-#include <cstdio>
-#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <string>
 using namespace std;
-typedef long long ll;
 void solve() {
-  ll n;
-  scanf("%lld", &n);
-  ll arr[n];
-  for (ll i = 0; i < n; i++) {
-    cin >> arr[i];
-  }
-  if (n % 2 == 1) {
-    printf("NO\n");
-    return;
-  }
-  ll odd = LLONG_MIN;
-  ll even = LLONG_MAX;
-  bool cant = false;
-  for (ll i = 0; i < n; i++) {
-    if (i % 2 == 0) {
-      even = min(even, arr[i]);
-    } else {
-      odd = max(odd, arr[i]);
-      if (arr[i] > arr[i - 1] || abs(arr[i] - arr[i - 1]) == 1) {
-        cant = true;
-      }
+  string s;
+  cin >> s;
+  int n = s.length();
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '0') {
+      s.erase(i, 1);
+      break;
     }
   }
-  if (abs(even - odd) < 2 || cant || even < odd) {
-    cout << "NO\n";
-  } else {
-    cout << "YES\n";
+  for (int i = 0; i < n; i++) {
+    if (s[i] == '1') {
+      s.erase(i, 1);
+      break;
+    }
   }
+  cout << s << endl;
 }
 int main() {
-  ll t;
-  scanf("%lld", &t);
+  int t;
+  std::cin >> t;
   while (t--) {
     solve();
   }
