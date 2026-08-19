227745a040ecc4445f5c0c158e0420d6fc3218b8
RadingkaRochaArfian <radingka.arfian@gmail.com>
2026-08-19 12:44:52 +0700

    add 709A implementation and link

--- codeforces/709A_Juicer.py
+++ codeforces/709A_Juicer.py
@@ -1,2 +1,12 @@
 n,b,d=map(int,input().split())
 arr=list(map(int,input().split()))
+sum=0
+cnt=0
+for x in arr:
+    if x>b:
+        continue
+    sum+=x
+    if sum>d:
+        cnt+=1
+        sum=0
+print(cnt)
